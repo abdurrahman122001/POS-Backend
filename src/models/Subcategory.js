b52818f4645fce3320d4961edea6f99066ff3cf9
Abdur Rahman <qaziabdurrahman12@gmail.com>
2025-09-05 17:22:23 +0500

    updated code

--- src/models/Subcategory.js
+++ src/models/Subcategory.js
@@ -1,26 +1,23 @@
 const mongoose = require("mongoose");
 
-
 const SubcategorySchema = new mongoose.Schema(
-{
-name: { type: String, required: true, trim: true },
-description: { type: String, default: "" },
-image: { type: String }, // optional banner image
-isActive: { type: Boolean, default: true },
-category: {
-type: mongoose.Schema.Types.ObjectId,
-ref: "Category",
-required: true,
-index: true,
-},
-},
-{ timestamps: true }
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, default: "" },
+    image: { type: String }, // optional banner image
+    isActive: { type: Boolean, default: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
 );
-
 
 // Unique name per category
 SubcategorySchema.index({ name: 1, category: 1 }, { unique: true });
 SubcategorySchema.index({ name: 1 });
-
 
 module.exports = mongoose.model("Subcategory", SubcategorySchema);

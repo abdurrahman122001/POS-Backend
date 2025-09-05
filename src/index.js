d405987cfb625b1f82d8937390239f55f22bb732
Abdur Rahman <qaziabdurrahman12@gmail.com>
2025-09-05 12:25:56 +0500

    updated code

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));app.use(express.json());
 
 // DB Connection
 mongoose.connect(process.env.MONGODB_URI, {
